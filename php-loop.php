678b829368355c5d9a010ad395cbc43ce7ff70fe
caneomarkhel <caneomarkhel@gmail.com>
2026-02-12 23:35:46 +0800

    Add files via upload

---
+++ php-loop.php
@@ -0,0 +1,39 @@
+<?php
+
+// Exercise 1
+for($evenNum = 0; $evenNum <= 20; $evenNum++) 
+{
+    if($evenNum % 2 == 0) 
+    {
+        echo "Numbers: " . $evenNum . '<br>';
+    }
+}
+
+// Exercise 2
+$groceries = ["Chocolate", "Bread", "Mango", "Potatoes", "Eggs"];
+
+echo "<br>";
+echo "Grocery List:" . '<br><br>';
+
+foreach($groceries as $grocery) {
+    echo $grocery . '<br>';
+}
+
+echo '<br><br>';
+
+// Exercise 3
+$countdown = 10;
+
+while($countdown >= 0) 
+{
+    echo $countdown . "..." . '<br>';
+    $countdown--;
+
+    if($countdown == 0) 
+    {
+        echo "BLAST OFF!";
+        break;
+    }
+}
+
+?>
